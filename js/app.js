ecc07da4c3275837dc6aff66fbd2f86194443cd8
Felixnite <naolizuniga@gmail.com>
2025-07-28 06:43:09 -0400

    Starting js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//Definir las variables o selectores
+const carrito = document.querySelector('#carrito')
+const listaTreats = document.querySelector('#lista-treats')
+const contenedorCarrito = document.querySelector('#lista-carrito tbody')
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito')
+let articuloCarrito = []
